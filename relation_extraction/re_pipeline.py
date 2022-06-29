b9aadd913027f08e78eaf54c7ee654def57ebe98
edchengg <ychen3411@gatech.edu>
2022-06-29 16:33:09 -0400

    Add files via upload

---
+++ relation_extraction/re_pipeline.py
@@ -0,0 +1,94 @@
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer, AutoModelForSequenceClassification
+
+ner_model = AutoModelForTokenClassification.from_pretrained("ychenNLP/arabic-ner-ace")
+ner_tokenizer = AutoTokenizer.from_pretrained("ychenNLP/arabic-ner-ace")
+ner_pip = pipeline("ner", model=ner_model, tokenizer=ner_tokenizer, grouped_entities=True)
+
+re_model = AutoModelForSequenceClassification.from_pretrained("ychenNLP/arabic-relation-extraction")
+re_tokenizer = AutoTokenizer.from_pretrained("ychenNLP/arabic-relation-extraction")
+re_pip = pipeline("text-classification", model=re_model, tokenizer=re_tokenizer)
+
+
+def process_ner_output(entity_mention, inputs):
+    re_input = []
+    for idx1 in range(len(entity_mention) - 1):
+        for idx2 in range(idx1 + 1, len(entity_mention)):
+            ent_1 = entity_mention[idx1]
+            ent_2 = entity_mention[idx2]
+
+            ent_1_type = ent_1['entity_group']
+            ent_2_type = ent_2['entity_group']
+            ent_1_s = ent_1['start']
+            ent_1_e = ent_1['end']
+            ent_2_s = ent_2['start']
+            ent_2_e = ent_2['end']
+            new_re_input = ""
+            for c_idx, c in enumerate(inputs):
+                if c_idx == ent_1_s:
+                    new_re_input += "<{}>".format(ent_1_type)
+                elif c_idx == ent_1_e:
+                    new_re_input += "</{}>".format(ent_1_type)
+                elif c_idx == ent_2_s:
+                    new_re_input += "<{}>".format(ent_2_type)
+                elif c_idx == ent_2_e:
+                    new_re_input += "</{}>".format(ent_2_type)
+                new_re_input += c
+            re_input.append({"re_input": new_re_input, "arg1": ent_1, "arg2": ent_2, "input": inputs})
+    return re_input
+
+
+def post_process_re_output(re_output, text_input, ner_output):
+    final_output = []
+    for idx, out in enumerate(re_output):
+        if out["label"] != 'O':
+            tmp = re_input[idx]
+            tmp['relation_type'] = out
+            tmp.pop('re_input', None)
+            final_output.append(tmp)
+
+    template = {"input": text_input,
+                "entity": ner_output,
+                "relation": final_output}
+
+    return template
+
+
+text_input = 'قال وزير العدل التركي بكير بوزداغ إن أنقرة تريد 12 مشتبهاً بهم من فنلندا و 21 من السويد'
+ner_output = ner_pip(text_input)  # inference NER tags
+
+re_input = process_ner_output(ner_output, text_input)  # prepare a pair of entity and predict relation type
+
+re_output = []
+for idx in range(len(re_input)):
+    tmp_re_output = re_pip(re_input[idx]["re_input"])  # for each pair of entity, predict relation
+    re_output.append(tmp_re_output[0])
+
+re_ner_output = post_process_re_output(re_output, text_input, ner_output)  # post process NER and relation predictions
+print("Sentence: ", re_ner_output["input"])
+print('====Entity====')
+for ent in re_ner_output["entity"]:
+    print('{}--{}'.format(ent["word"], ent["entity_group"]))
+print('====Relation====')
+for rel in re_ner_output["relation"]:
+    print('{}--{}:{}'.format(rel['arg1']['word'], rel['arg2']['word'], rel['relation_type']['label']))
+
+'''
+Sentence:  قال وزير العدل التركي بكير بوزداغ إن أنقرة تريد 12 مشتبهاً بهم من فنلندا و 21 من السويد
+====Entity====
+وزير--PER
+العدل--ORG
+التركي--GPE
+بكير بوزداغ--PER
+انقرة--GPE
+مشتبها بهم--PER
+فنلندا--GPE
+21--PER
+السويد--GPE
+====Relation====
+وزير--العدل:ORG-AFF
+مشتبها بهم--فنلندا:PHYS
+21--السويد:PHYS
+'''
+
+
+
